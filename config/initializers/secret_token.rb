e9cb52e75afc067e121e5a970470bf3429b0e94b
JShelto <jshelto@sas.upenn.edu>
2013-07-12 14:35:40 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'b279775837ddec7413f72c4da83083d2d1cbb226a67fc4e1ed3acea36f855523db35c91e431274a9c5fd3d634d47e74719ecc365f2a8e2215d2e81e22657c49d'
